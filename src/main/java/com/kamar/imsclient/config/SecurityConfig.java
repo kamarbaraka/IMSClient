a93a9fae37a4f3e486d59a1c8fe2c2670f364b00
kamarbaraka <kamar254baraka@gmail.com>
2023-11-28 02:33:17 +0300

    config
    Signed-off-by: kamarbaraka <kamar254baraka@gmail.com>

--- src/main/java/com/kamar/imsclient/config/SecurityConfig.java
+++ src/main/java/com/kamar/imsclient/config/SecurityConfig.java
@@ -1,2 +1,32 @@
-package com.kamar.imsclient.config;public class SecurityConfig {
+package com.kamar.imsclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * the security configuration.
+ * @author kamar baraka.*/
+
+
+@Configuration
+public class SecurityConfig  {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+
+        /*set the authentication method*/
+        httpSecurity.oauth2Login(oauth2 -> {});
+
+        ClientRegistration githubClientRegistration = CommonOAuth2Provider.GITHUB.getBuilder("github")
+                .clientId("8dd8830b0ae03146ca71")
+                .clientSecret("899f7b5aad76fb972ddd1d87d1c452425b365dba").build();
+        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+
+        return httpSecurity.build();
+    }
 }
--- src/main/java/com/kamar/imsclient/home/controller/HomeController.java
+++ src/main/java/com/kamar/imsclient/home/controller/HomeController.java
@@ -1,2 +1,30 @@
-package com.kamar.imsclient.home.controller;public class HomeController {
+package com.kamar.imsclient.home.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * the home page controller.
+ * @author kamar baraka.*/
+
+
+@Controller
+@RequestMapping(value = {"/", "/home"})
+public class HomeController {
+
+
+    @GetMapping
+    public String home(@AuthenticationPrincipal UserDetails authenticatedUser, Model model){
+
+        /*add username attribute to the view*/
+        String username = authenticatedUser.getUsername();
+        model.addAttribute("username", username);
+
+        return "home";
+
+    }
 }
